95e04aa452216285cf3ec327306c7a66f88c1e9b
andrielhandrail <69703685+andrielhandrail@users.noreply.github.com>
2020-10-25 17:27:16 +1300

    Add files via upload

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,8 @@
-"""This is where the web application is opened."""
-
+"""App entry point."""
 from aperflix import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, threaded=False)
+
