5cab5ebf38db59a0575a10f6fc729a5cecbd3105
andrielhandrail <69703685+andrielhandrail@users.noreply.github.com>
2020-10-25 17:26:29 +1300

    Add files via upload

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
-"""Load the environment variables from .env."""
-
-from os import environ
+"""Flask configuration variables."""
+from os import environ, path, getenv
 from dotenv import load_dotenv
 
+# Load environment variables from file .env, stored in this directory.
 load_dotenv()
 
 
 class Config:
+    """Set Flask configuration from .env file."""
+
+    # Flask configuration
     FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
     SECRET_KEY = environ.get('SECRET_KEY')
